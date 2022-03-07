498a49674aca1776cbdbe7c046fd98a116e12ee6
Ralitsa Atanasova <97174296+ralitsaatanasova21@users.noreply.github.com>
2022-03-07 18:43:06 +0200

    Delete 01. List-Of-Items.js

--- 01. List-Of-Items.js
+++
@@ -1,8 +0,0 @@
-function addItem() {
-  let inputElement = document.getElementById("newItemText");
-  let listItems = document.getElementById("items");
-
-  let newElement = document.createElement("li");
-  newElement.textContent = inputElement.value;
-  listItems.appendChild(newElement);
-}
